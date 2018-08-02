eb6d5446345ddc259b41082e20c6e01b9b5b9ee6
Max Brereton <max.brereton@gmail.com>
2018-08-02 15:45:01 +1000

    Remove old code and start handling removal of arcs from the beachline (circle events)

--- redblacktree.go
+++ redblacktree.go
@@ -52,7 +52,8 @@ func (n *node) insert(currentNode *node, newKey int, newSite *site, eventQueue *
 		// The 3 leaf nodes that represent the arcs
 		leftLeafNode := node{arcSite: currentNode.arcSite, previous: currentNode.previous, key: currentNode.key}
 		middleLeafNode := node{arcSite: newSite, previous: &leftLeafNode, key: newKey}
-		rightLeafNode := node{arcSite: currentNode.arcSite, next: currentNode.next, previous: &middleLeafNode, key: currentNode.key}
+		rightLeafNode := node{arcSite: currentNode.arcSite, next: currentNode.next, previous: &middleLeafNode,
+			key: currentNode.key}
 		middleLeafNode.next = &rightLeafNode
 		leftLeafNode.next = &middleLeafNode
 
@@ -89,79 +90,23 @@ func (n *node) insert(currentNode *node, newKey int, newSite *site, eventQueue *
 	return currentNode
 }
 
-func (rbtree *redblacktree) delete(keyToRemove int) *node {
-	if rbtree.root == nil {
-		return nil
+func (rbtree *redblacktree) removeArc(leafNode *node, eventQueue *PriorityQueue) {
+	if leafNode.parent == nil {
+		// This happens if only 1 node is in the tree and you call remove - this should never happen
+		rbtree.root = nil
 	}
-	return rbtree.root.delete(rbtree.root, keyToRemove)
+
+	leftLeafNode := leafNode.previous
+	rightLeafNode := leafNode.next
+
+	// Check if left or right have circle events - these won't be valid once leaf node has been removed
+	if leftLeafNode.circleEvent != nil {
+		heap.Remove(eventQueue, leftLeafNode.circleEvent.index)
+	}
+	if rightLeafNode.circleEvent != nil {
+		heap.Remove(eventQueue, rightLeafNode.circleEvent.index)
+	}
 }
-
-func (n *node) delete(currentNode *node, keyToRemove int) *node {
-	// Base case
-	if currentNode == nil {
-		return currentNode
-	}
-
-	if keyToRemove < currentNode.key {
-		currentNode.left = currentNode.delete(currentNode.left, keyToRemove)
-
-	} else if keyToRemove > currentNode.key {
-		currentNode.right = currentNode.delete(currentNode.right, keyToRemove)
-
-	} else {
-
-		// If node only has one child or no children
-		if currentNode.left == nil {
-			return currentNode.right
-		} else if currentNode.right == nil {
-			return currentNode.left
-		}
-
-		// handle case where the node to delete has two children
-		// Get smallest node in right subtree (ie. inorder successor to current node)
-		inorderSuccessor := currentNode.minValueNode(currentNode.right)
-		// Copy it's value to the current node
-		currentNode.key = inorderSuccessor.key
-		// Delete the inorder successor
-		currentNode.right = currentNode.delete(currentNode.right, inorderSuccessor.key)
-	}
-
-	return currentNode
-}
-
-// Find the node with minimum value given a binary search tree
-func (n *node) minValueNode(currentNode *node) *node {
-	if currentNode.left != nil {
-		return currentNode.minValueNode(currentNode.left)
-	}
-	return currentNode
-}
-
-// // Return the minimum value leaf node in the subtree
-// func minValueLeafNode(currentNode *node) *node {
-// 	if currentNode.breakpoint != nil {
-// 		if currentNode.left != nil {
-// 			return minValueLeafNode(currentNode.left)
-// 		} else if currentNode.right != nil {
-// 			return minValueLeafNode(currentNode.right)
-// 		}
-// 		// nothing should get here - if it does it is an error - all internal nodes should have at least one child
-// 	}
-// 	return currentNode
-// }
-
-// // Return the maximum value leaf node in the subtree
-// func maxValueLeafNode(currentNode *node) *node {
-// 	if currentNode.breakpoint != nil {
-// 		if currentNode.right != nil {
-// 			return maxValueLeafNode(currentNode.right)
-// 		} else if currentNode.left != nil {
-// 			return maxValueLeafNode(currentNode.left)
-// 		}
-// 		// error - all internal nodes should have at least one child
-// 	}
-// 	return currentNode
-// }
 
 func (rbtree *redblacktree) inorderTraversal() {
 	rbtree.root.inorderTraversal(rbtree.root)
